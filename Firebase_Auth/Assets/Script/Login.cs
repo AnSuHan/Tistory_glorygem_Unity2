470efe41f7571adf9fdd9605f40532f6e89b3ebc
AnSuHan <glorygem195@gmail.com>
2024-03-24 18:00:27 +0900

    email login

---
+++ Firebase_Auth/Assets/Script/Login.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+
+public class Login : MonoBehaviour
+{
+    private FirebaseAuth auth;
+
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            if (task.Result == DependencyStatus.Available)
+            {
+                auth = FirebaseAuth.DefaultInstance;
+            }
+            else
+            {
+                Debug.LogError("Could not resolve Firebase dependencies: " + task.Exception);
+                return;
+            }
+        });
+    }
+
+    public void Register_Email()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync("inputEmail@gmail.com", "inputPassword");
+    }
+
+    public void Login_Email()
+    {
+        auth.SignInWithEmailAndPasswordAsync("inputEmail@gmail.com", "inputPassword");
+    }
+}
